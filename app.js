a485fd956ae67e28fbdc6005975a21f7dc35dd1b
vikash.tanwer-at-047117452485 <vikash.tanwer-at-047117452485>
2019-10-15 11:15:41 +0530

    change in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expresshandle = require('express-handlebars');
 const app = express();
+const port =process.env.PORT || 3000;
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -31,4 +32,4 @@ app.use((req, res, next) => {
     res.status(404).render('404',{pagetitle:'page not found'});
 });
 
-app.listen(3000);
+app.listen(port);
